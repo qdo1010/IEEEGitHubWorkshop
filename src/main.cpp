372540bb2958efb288f4e815ed4386be4960cabf
qhdo1010 <do.q@husky.neu.edu>
2016-03-28 19:15:22 -0400

    i make changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main(){
 	cout <<"Hi Quan"<<endl;
-	cout <<“ HELLO” <<endl;
 	cout << "hello world" <<endl;
 	cout << "Hello Kenneth"<<endl;
+	int a = 1;
+	int b = 2;
+	int sum;
+	sum = add(a,b);
+	cout << sum << endl;
 	return 0;
+}
+
+int add(int a, int b){
+	return a + b;
 }
